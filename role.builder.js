05dd4a6a38b3931bfa65e7ac098a178a212a511b
JonathanElm <47280380+JonathanElm@users.noreply.github.com>
2020-01-04 10:41:17 -0800

    Create role.builder.js

---
+++ role.builder.js
@@ -0,0 +1,35 @@
+var roleBuilder = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+      //Checks if the creep should return to its harvesting state.
+	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('🔄 harvest');
+	    }
+      //Checks if the creep should return to its building state.
+	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.building = true;
+	        creep.say('🚧 build');
+	    }
+      
+      //If the creep is in the building state it should move to a build site and start building.
+	    if(creep.memory.building) {
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+	    }
+      //Has the creep move to harvesting sites.
+	    else {
+	        var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+	    }
+	}
+};
+
+module.exports = roleBuilder;
