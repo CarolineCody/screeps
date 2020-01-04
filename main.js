7291b88b393532f2430d9ef0b4c8024d9230be8e
JonathanElm <47280380+JonathanElm@users.noreply.github.com>
2020-01-04 10:48:29 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,18 @@
-var roleHarvester = {
-    //default function call for the creep.
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        //If the creep still has inventory space.
-	    if(creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
-        else {
-            //Looks for structure extensions to the spond to fill spaces.
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-            });
-            //Transfers resources out.
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-        }
-	}
-};
+var roleHarvester = require('role.harvester');
+var roleBuilder = require('role.builder');
 
-module.exports = roleHarvester;
+module.exports.loop = function () {
+    //Logs available resources.
+    for(var name in Game.rooms) {
+        console.log('Room "'+name+'" has '+Game.rooms[name].energyAvailable+' energy');
+    }
+    //GIves each creep their necessary roles.
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
