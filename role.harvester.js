10e13ecd7f143cd7ea06ffd2c5e261130b493756
JonathanElm <47280380+JonathanElm@users.noreply.github.com>
2020-01-04 11:12:10 -0800

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -2,17 +2,22 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        //Checks if the creep should just take more resources.
 	    if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
+        //The creep has more stuff to collect.
         else {
+            //Finds the structures that need to be repaired.
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN ||
+                                structure.structureType == STRUCTURE_TOWER) && 
+                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
             if(targets.length > 0) {
