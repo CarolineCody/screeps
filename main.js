630a37fbec8ded0e7d628e952ebada82bef4f7fe
JonathanElm <47280380+JonathanElm@users.noreply.github.com>
2020-01-04 10:14:59 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-//Enter this in the console to print something out.
+//Enter this in the console to make a standard worker creep.
 //Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+
+//Enter this in the consoel to maek an upgrader creep.
+//Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Upgrader1' );
+//How to assign a role to the upgrader.
+//Game.creeps['Harvester1'].memory.role = 'harvester';
+//Game.creeps['Upgrader1'].memory.role = 'upgrader';
 
 //Creates an object of the branch of role.harvester.
 var roleHarvester = require('role.harvester');
