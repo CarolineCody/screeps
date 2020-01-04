56261c80b8902f90e312fe01eed13e6c873022a8
JonathanElm <47280380+JonathanElm@users.noreply.github.com>
2020-01-04 10:48:03 -0800

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -1,19 +1,27 @@
 var roleHarvester = {
-
+    //default function call for the creep.
     /** @param {Creep} creep **/
-    //primary function call of the role harvester program.
     run: function(creep) {
-        //If the creep can harvest more material the creep should.
-	      if(creep.store.getFreeCapacity() > 0) {
+        //If the creep still has inventory space.
+	    if(creep.store.getFreeCapacity() > 0) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
-        //If the creep cannot carry anything else the creep should transfer its resources to the spawn.
-        else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
-            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+        else {
+            //Looks for structure extensions to the spond to fill spaces.
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+            });
+            //Transfers resources out.
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
             }
         }
 	}
