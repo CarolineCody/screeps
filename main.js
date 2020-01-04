da266eefe42eaea8296e8db687264c89e578256f
JonathanElm <47280380+JonathanElm@users.noreply.github.com>
2020-01-04 11:10:52 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,36 @@
-//Has creep remove something there.
+//Has creep remove itself from game.
 //Game.creeps['Harvester1'].suicide()
 
-//Oranges unit type with neessary scripts.
+//Has the map become a stable map with no one with weapons in your room. 
+//Game.spawns['Spawn1'].room.controller.activateSafeMode();
+
+//Makes a tower at a given location.
+//Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
+
+//Ensures the program has to do the base functions it needs to do.
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
+    
+    //Gets the tower to preform any defense requirements and repair all structures.
+    var tower = Game.getObjectById('299f364376fda88b941461db');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
 
-    //Removes the memory allocation of dead creeps.
-    for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
         }
     }
-    
-    //Logs the number of creeps on the screen.
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
 
-    //If the game has less than two harvesters it should update the count.
-    if(harvesters.length < 2) {
-        var newName = 'Harvester' + Game.time;
-        console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
-            {memory: {role: 'harvester'}});
-    }
-    
-    //Creates the visual that the spawn is making a new creep.
-    if(Game.spawns['Spawn1'].spawning) { 
-        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-        Game.spawns['Spawn1'].room.visual.text(
-            '🛠️' + spawningCreep.memory.role,
-            Game.spawns['Spawn1'].pos.x + 1, 
-            Game.spawns['Spawn1'].pos.y, 
-            {align: 'left', opacity: 0.8});
-    }
-    
-    //Has each creep doing its job.
+    //Makes sure each creep gets stuff done.
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
@@ -45,6 +38,9 @@ module.exports.loop = function () {
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
 }
