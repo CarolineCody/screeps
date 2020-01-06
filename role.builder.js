857d127602df3b491f3fdc953dace7c98a11c44c
JonathanElm <47280380+JonathanElm@users.noreply.github.com>
2020-01-06 07:53:24 -0800

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -1,1 +1,37 @@
+var roleBuilder = {
 
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+      //Updates the state of the creep to say that it is harvesting because it is out of energy.
+	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('🔄 harvest');
+	    }
+      
+      //Updates the state of the creep to say it is building something.
+	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.building = true;
+	        creep.say('🚧 build');
+	    }
+
+      //If the creep has something to build, it will start building things.
+	    if(creep.memory.building) {
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+	    }
+      //Otherwise the creep just goes on to build something.
+	    else {
+	        var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+	    }
+	}
+};
+//Passes on its role.
+module.exports = roleBuilder;
