4b1a86844f742fdd7719fe5ffafa362a9ce93361
JonathanElm <47280380+JonathanElm@users.noreply.github.com>
2020-01-06 07:58:35 -0800

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,1 +1,33 @@
+var roleUpgrader = {
 
+    /** @param {Creep} creep **/
+    run: function(creep) {
+      
+      //Updates the creeps state to harvest more resources.
+      if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.upgrading = false;
+            creep.say('🔄 harvest');
+	    }
+      //Updates the creeps state to upgrade the room controller.
+	    if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.upgrading = true;
+	        creep.say('⚡ upgrade');
+	    }
+
+      //Preforms the upgrade action onto the room controller.
+	    if(creep.memory.upgrading) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+      }
+      //Preforms the harvest action on the nearest source.
+      else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+      }
+	}
+};
+//Adds a module of the name "roleUpgrader".
+module.exports = roleUpgrader;
