1904b660ed0218f86c2e0c81b72fb67efc7f07bd
JonathanElm <47280380+JonathanElm@users.noreply.github.com>
2020-01-07 05:42:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -29,6 +29,41 @@ module.exports.loop = function () {
             tower.attack(closestHostile);
         }
     }
+    
+    
+    //Variable type that logs the amount of each type of creep in game.
+    var builder = _.filter(Game.creeps,(creep) => creeep.memory.role == 'builder');
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    var upgraders = _.filter(Game.creeps,(creep) => creep.memory.role == 'upgrader');
+
+    //If the game has less than two harvesters it should update the count.
+    if(Game.spawns['Spawn1'].spawning && upgraders.length < 1){
+        var newName = 'Upgrader' + Game.time;
+        console.log('Spawning new upgrader: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+        {memory: {role: 'upgrader'}});
+    }
+    else if(Game.spawns['Spawn1'].spawning && harvesters.length < 2) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+            {memory: {role: 'harvester'}});
+    }
+    else if(Game.spawns['Spawn1'].spawning && builder.length < 2){
+        var newName = 'Builder' + Game.time;
+        console.log('Spawning new builder: ' + newName);
+        Game.spawns['spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, { memory: {role: 'builder'}})
+    }
+    
+    //Creates the visual that the spawn is making a new creep.
+    if(Game.spawns['Spawn1'].spawning) { 
+        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1, 
+            Game.spawns['Spawn1'].pos.y, 
+            {align: 'left', opacity: 0.8});
+    }
 
     //Makes sure each creep gets stuff done.
     for(var name in Game.creeps) {
